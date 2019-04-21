42b128e919eb1bc9aea2a9f03f29954cd726dfc7
cadu <carlosvicc14@gmail.com>
2019-04-21 20:17:34 -0300

    adicionando novo remoteMethod ao tarefas

--- common/models/tarefas.js
+++ common/models/tarefas.js
@@ -1,5 +1,29 @@
 'use strict';
 
 module.exports = function(Tarefas) {
+  Tarefas.createNew = function(data) {
+    let newCustomerId;
+    let equilibrios = null;
 
+    if (data.equilibrios && data.equilibrios.length) {
+      equilibrios = data.equilibrios;
+    }
+
+    return Tarefas
+      .create(data);
+  };
+  Tarefas.remoteMethod('createNew', {
+    accepts: [
+      {arg: 'id', type: 'string', required: true},
+      {arg: 'nome', type: 'string'},
+      {arg: 'duracao', type: 'string'},
+      {arg: 'data', type: 'date'},
+      {arg: 'statusId', type: 'object'},
+      {arg: 'equilibrios', type: 'array'},
+      {arg: 'metas', type: 'array'},
+      {arg: 'papeis', type: 'array'},
+      {arg: 'triade', type: 'object'},
+    ],
+    returns: {arg: 'response', type: 'string'},
+  });
 };
